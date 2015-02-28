d018169b38b5e7d49aa7d452de9c15a6e5f9d0d9
Thomas Voogt <chirimorin@gmail.com>
2015-02-28 16:30:44 +0100

    Todos bekijken (toevoegen werkt niet goed op mobiel, wel op browser)

---
+++ www/js/index.js
@@ -0,0 +1,26 @@
+function displayTodos(tx, rs) {
+    // Maak een string voor de lijst
+    var list = "";
+
+    // Loop door alle resultaten
+    for (i = 0; i < rs.rows.length; i++) {
+        // resultaat ophalen
+        var row = rs.rows.item(i);
+
+        // Voeg de todo toe aan de lijst
+        list += '<li class="note"><span class="naam">' + row.gebruiker + '</span><span class="datum">' + row.aangemaaktOp.split("GMT")[0] + '</span><br /><span class="bericht">' + row.beschrijving + '</span></li>'
+    }
+
+    // Verwijder bestaande notes en voeg de gevonden notes toe aan de lijst.
+    $(".note").remove();
+    $("#notes").append(list).listview().listview("refresh");
+}
+
+$(document).ready(function() {
+    loadDatabase();
+
+    dbShell.transaction(function(tx) {
+        tx.executeSql("SELECT * from todos", [], displayTodos);
+    }, dbErrorHandler);
+});
+
