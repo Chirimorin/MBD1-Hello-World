490f29caa6e9c500c65812830c0b5333a73610c2
Thomas Voogt <chirimorin@gmail.com>
2015-03-03 14:01:17 +0100

    Opdracht goud gefixed + comments

--- www/js/global.js
+++ www/js/global.js
@@ -1,26 +1,31 @@
+// Slaat een object op in localstorage.
 function save(name,value) {
     localStorage.setItem(name, JSON.stringify(value));
 }
 
+// Laad een object uit localstorage.
 function load(name) {
     return JSON.parse(localStorage.getItem(name));
 }
 
-
+// WebSQL database laden
 var dbShell;
 function loadDatabase() {
     dbShell = window.openDatabase("ToDos", 1, "ToDos", 1000000);
     dbShell.transaction(setupTable,dbErrorHandler);
 }
 
+// Tabel opzetten voor webSQL
 function setupTable(tx) {
     tx.executeSql("CREATE TABLE IF NOT EXISTS todos(id INTEGER PRIMARY KEY, gebruiker, beschrijving, aangemaaktOp)");
 }
 
+// Error callback voor WebSQL
 function dbErrorHandler(err) {
     alert("DB Error: "+err.message + "\nCode="+err.code);
 }
 
+// Laad de database als de pagina klaar is met laden
 $(document).ready(function() {
     loadDatabase();
 });
--- www/js/new.js
+++ www/js/new.js
@@ -1,13 +1,16 @@
 function TodoOpslaan() {
+    // Laad de waarden van de velden.
     var gebruiker = $("#gebruiker").val();
     var beschrijving = $("#beschrijving").val();
-    
+
+    // Controleer of er waarden zijn
     if (gebruiker == "" || beschrijving == "")
     {
         alert("Vul een gebruiker en beschrijving in");
     }
     else
     {
+        // Insert de waarden in de webSQL database
         dbShell.transaction(function(tx) {
             tx.executeSql("insert into todos(gebruiker,beschrijving,aangemaaktOp) values(?,?,?)", [gebruiker, beschrijving, new Date()]);
         }, dbErrorHandler, OpslaanGelukt);
@@ -15,25 +18,25 @@ function TodoOpslaan() {
 }
 
 function OpslaanGelukt() {
+    // Leeg de velden
     $("#gebruiker").val("");
     $("#beschrijving").val("");
-    
+
+    // Redirect naar index, hierbij worden automatish de localStorage waarden leeggemaakt (door unload event)
     window.location = "index.html";
 }
 
 $(document).ready(function() {
+    // Laad eventueel opgeslagen waarden
+    $("#gebruiker").val(load("gebruiker"));
+    $("#beschrijving").val(load("beschrijving"));
+
+    // Bind de event handler aan de toevoegen knop
     $("#toevoegen").click(TodoOpslaan);
 });
 
-
-// Werkt niet
-$(document).on("pagebeforehide","#new",function(){
+$(window).unload(function(){
+    // Sla de waarden van de to-do velden op in localStorage
 	save("gebruiker", $("#gebruiker").val());
 	save("beschrijving", $("#beschrijving").val());
-});
-
-// Werkt niet
-$(document).on("pagebeforeshow","#new",function(){
-	$("#gebruiker").val(load("gebruiker"));
-    $("#beschrijving").val(load("beschrijving"));
 });
